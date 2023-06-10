711492eff50ec4f6a218a9a9b13d6b0989ab9c3d
vagisha15 <116742165+vagisha15@users.noreply.github.com>
2023-06-11 03:14:50 +0530

    Update menu.js

--- static/menu.js
+++ static/menu.js
@@ -7,31 +7,63 @@ fetch('/templates/menu.html')
     // Insert the menu.html content into the 'menu' div
     document.getElementById('menu').innerHTML = data;
 
-    // Add event listener to the 'Writer' button
-    var writerButton = document.getElementById('writer-button');
-    writerButton.addEventListener('click', function() {
-      // Open the desired page when the button is clicked
-      window.location.href = '/templates/writer.html';
-    });
+    // Get the username from the Flask application
+    fetch('/get_username')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var username = data.username;
 
-    var homeButton = document.getElementById('reader-button');
-    homeButton.addEventListener('click', function() {
-      // Open the desired page when the button is clicked
-      window.location.href = '/';
-    });
+        // Add event listener to the 'Writer' button
+        var writerButton = document.getElementById('writer-button');
+        writerButton.addEventListener('click', function() {
+          // Check if the username is not empty
+          if (username !== "") {
+            // Open the desired page when the button is clicked
+            window.location.href = '/templates/writer.html';
+          } else {
+            // Display a message asking the user to login first
+            alert('Please login first');
+          }
+        });
 
-    var loginButton = document.getElementById('login-button');
-    loginButton.addEventListener('click', function() {
-      // Open the desired page when the button is clicked
-      window.location.href = '/templates/login.html';
-    });
+        // Rest of the code...
 
-    var signupButton = document.getElementById('signup-button');
-    signupButton.addEventListener('click', function() {
-      // Open the desired page when the button is clicked
-      window.location.href = '/templates/signup.html';
-    });
+        var homeButton = document.getElementById('reader-button');
+        homeButton.addEventListener('click', function() {
+          // Open the desired page when the button is clicked
+          window.location.href = '/';
+        });
 
+        var loginButton = document.getElementById('login-button');
+        loginButton.addEventListener('click', function() {
+          // Open the desired page when the button is clicked
+          window.location.href = '/templates/login.html';
+        });
+
+        var signupButton = document.getElementById('signup-button');
+        signupButton.addEventListener('click', function() {
+          // Open the desired page when the button is clicked
+          window.location.href = '/templates/signup.html';
+        });
+
+        var logo = document.getElementById('logo');
+        logo.addEventListener('click', function() {
+          // Open the desired page when the button is clicked
+          window.location.href = '/';
+        });
+
+        var aboutButton = document.getElementById('contact-button');
+        aboutButton.addEventListener('click', function() {
+          // Open the desired page when the button is clicked
+          window.location.href = '/templates/about.html';
+        });
+
+      })
+      .catch(function(error) {
+        console.error('Error:', error);
+      });
   })
   .catch(function(error) {
     console.error('Error:', error);
